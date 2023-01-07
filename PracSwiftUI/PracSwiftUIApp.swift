a0bde5debee6aeb6d2c66a5c7ac34b5c1be9655f
Conner Kim <dudrjs145@naver.com>
2023-01-07 21:29:37 +0900

    Initial Commit

---
+++ PracSwiftUI/PracSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracSwiftUIApp.swift
+//  PracSwiftUI
+//
+//  Created by Conner on 2023/01/07.
+//
+
+import SwiftUI
+
+@main
+struct PracSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
